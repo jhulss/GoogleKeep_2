81cd744281b6c5292cf3d909b55f9b8cb87a06d3
Fabio <dahofcmmejia@gmail.com>
2023-03-07 05:48:33 -0400

    Ahorra se pueden eliminar las notas

--- src/mostrar_lista_notas.js
+++ src/mostrar_lista_notas.js
@@ -1,17 +1,27 @@
 function mostrar_lista_notas(notas, div_notas) {
-    
+
     div_notas.innerHTML = '';
-    notas.forEach((item) => {
-        // console.log(item);
-        const itemNota = document.createElement('div');
-        itemNota.classList.add("item-nota");
-        itemNota.innerHTML = `
-        <p><strong>${item.titulo}:</strong>
-            <br>${item.descripcion}
-        </p>
-        `
-        div_notas.appendChild(itemNota);
-    })
+
+    if (notas.length > 0) {
+        notas.forEach((item) => {
+            // console.log(item);
+            const itemNota = document.createElement('div');
+            itemNota.classList.add("item-nota");
+            itemNota.innerHTML = `
+            <p><strong>${item.titulo}:</strong>
+                <br>${item.descripcion}
+            </p>
+            <div class="botones">
+                <button class="eliminar" data-id="${item.id}">Eliminar</button>
+            </div>
+            `
+            div_notas.appendChild(itemNota);
+        })
+    }else{
+        const mensaje = document.createElement('p');
+        mensaje.textContent = "Lista de notas vacia";
+        div_notas.appendChild(mensaje);
+    }
 
 }
 
--- src/presenter.js
+++ src/presenter.js
@@ -5,7 +5,7 @@ const titulo = document.querySelector("#titulo");
 const descripcion = document.querySelector("#descripcion");
 const form = document.querySelector("#nota-form");
 const div_notas = document.querySelector(".div_notas");
-const ul = document.querySelector('ul');
+
 const div_busqueda = document.querySelector("#resultado_busqueda_div");
 const form_busqueda = document.querySelector("#buscador-form");
 const buscar = document.querySelector("#buscar");
@@ -16,30 +16,30 @@ let notas = [];
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  // const li = document.createElement("li");
-  // const p = document.createElement("p");
-
   if(verificar_formulario(titulo.value, titulo_obligatorio)){
     return;
   }
-
   const objNota = {
     id: Date.now(),
     titulo:titulo.value,
     descripcion: descripcion.value,
   }
   notas = [...notas,objNota];
-
   form.reset();
   mostrar_lista_notas(notas, div_notas);
-  // p.innerHTML = "<p>" + "<strong>" + titulo.value + "</strong>" + "<br>"+ descripcion.value +"</p>";
-  
-  // li.appendChild(p);
-  // ul.appendChild(li);
 });
 
-form_busqueda.addEventListener("submit", (event) => {
-  event.preventDefault();
-  div_busqueda.innerHTML = "<p>" + LN.get(buscar.value) + "</p>";
+div_notas.addEventListener("click", (e)=>{
+  if (e.target.classList.contains('eliminar')){
+    const notaID = Number(e.target.getAttribute("data-id"));
+    const nuevasNotas = notas.filter((item) => item.id !== notaID);
+    notas = nuevasNotas;
+    mostrar_lista_notas(notas, div_notas)
+}
+
 });
+
+// form_busqueda.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   div_busqueda.innerHTML = "<p>" + LN.get(buscar.value) + "</p>";
+// });
